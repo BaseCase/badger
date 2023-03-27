8ad62542a45abbf89af927cfde14ce39a66033f1
Casey Brant <casey@caseybrant.com>
2023-03-27 16:12:37 -0500

    basic button and progress bar styles, starting interactivity

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,37 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+window.addEventListener('load', function() {
+  go()
+})
+
+function go() {
+  let new_badge_el = document.querySelector('#new-badge-container')
+  if (new_badge_el != null) {
+    new_badge_el.classList.remove('preload')
+    go_create_badge_page()
+  }
+}
+
+function go_create_badge_page() {
+  let state = {
+    is_button_down: false,
+    is_progress_bar_visible: false,
+  }
+
+  let big_button_el = document.querySelector('#the-big-button')
+  let progress_bar_container_el = document.querySelector('#progress-bar-container')
+
+  big_button_el.addEventListener('mousedown', big_button_down)
+  window.addEventListener('mouseup', big_button_up)
+
+  function big_button_down(event) {
+    if (!state.is_progress_bar_visible) {
+      progress_bar_container_el.classList.remove('hidden')
+      state.is_progress_bar_visible = true
+    }
+
+    state.is_button_down = true
+  }
+
+  function big_button_up(event) {
+    state.is_button_down = false
+  }
+}
