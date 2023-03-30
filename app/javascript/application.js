d9d9cdf9b04dcc52c0e4994c420acde1ed90157d
Casey Brant <casey@caseybrant.com>
2023-03-30 13:33:16 -0500

    New badge generation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import {init_new_badge_page} from "./new_badge_controls";
+
+
+window.addEventListener('load', function() {
+  let new_badge_container_el = document.querySelector('#new-badge-container')
+  if (new_badge_container_el != null) {
+    init_new_badge_page()
+  }
+
+  // hack to prevent css transitions from firing on initial load
+  let body = document.querySelector('body')
+  body.classList.remove('preload')
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get "/new", to: "badges#new"
 end
